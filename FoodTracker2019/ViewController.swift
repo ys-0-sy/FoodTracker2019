e528210e07a1392f02d3e7b40dc0fe04a3b4562f
Yuta Saito <saito@ssn.elec.keio.ac.jp>
2019-09-10 14:58:48 +0900

    Initial Commit

---
+++ FoodTracker2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker2019
+//
+//  Created by saito on 2019/09/10.
+//  Copyright © 2019 yuta saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
