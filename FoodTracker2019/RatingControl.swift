89dc180fda0465a8a34793ddd50770df0112b575
Yuta Saito <yuta.saito0703@gmail.com>
2019-09-13 18:36:05 +0900

    Add Accessibility Information

--- FoodTracker2019/RatingControl.swift
+++ FoodTracker2019/RatingControl.swift
@@ -89,6 +89,9 @@ import UIKit
             button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
             button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
+            // Set the accessibility label
+            button.accessibilityLabel = "Set ¥(index + 1) star rating"
+            
             // Stup the button action
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
             
@@ -97,6 +100,7 @@ import UIKit
             
             // Add te new button to the rating button array
             ratingButtons.append(button)
+
         }
         
         updateButtonSelectedStates()
@@ -106,6 +110,29 @@ import UIKit
         for (index, button) in ratingButtons.enumerated() {
             // If the index of a button is less than the ratig, that button should be selected.
             button.isSelected = index < rating
+            
+            // Set the hint string for the currently selected star
+            let hintString: String?
+            if rating == index + 1 {
+                hintString = "Tap to reset the rating to zero."
+            } else {
+                hintString = nil
+            }
+            
+            // Calculate the value string
+            let valueString: String
+            switch (rating) {
+            case 0:
+                valueString = "No rating set."
+            case 1:
+                valueString = "1 star set."
+            default:
+                valueString = "¥(rating) stars set."
+            }
+            
+            // Assign the nhint string and value string
+            button.accessibilityHint = hintString
+            button.accessibilityHint = valueString
         }
     }
 }
