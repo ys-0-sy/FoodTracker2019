9404e57257357ed80a66c73e9c9af138a3470e05
Yuta Saito <yuta.saito0703@gmail.com>
2019-09-17 01:02:16 +0900

    Create the Meal List

---
+++ MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker2019
+//
+//  Created by saito on 2019/09/17.
+//  Copyright © 2019 yuta saito. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
