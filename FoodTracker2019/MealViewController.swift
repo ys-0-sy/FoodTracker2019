dd1fd9c09084d59166fd01a83932683c1a440809
Yuta Saito <saito@ssn.elec.keio.ac.jp>
2019-09-18 14:08:17 +0900

    Store New Meals in the Meal List

--- FoodTracker2019/MealViewController.swift
+++ FoodTracker2019/MealViewController.swift
@@ -14,6 +14,13 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    /*
+     This value is either passed by `MealTableViewController` in `prepare(for:sender:)`
+     or constructed as part of adding a new meal
+    */
+    var meal: Meal?
     
     override func viewDidLoad() {
         super.viewDidLoad()
