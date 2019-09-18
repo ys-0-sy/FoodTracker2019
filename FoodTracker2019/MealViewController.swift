83a72d5be6547f51cf81ffaea143b6e6241f5de3
Yuta Saito <saito@ssn.elec.keio.ac.jp>
2019-09-19 00:07:10 +0900

    Disable Saving When the User Doesn't Enter an Item Name

--- FoodTracker2019/MealViewController.swift
+++ FoodTracker2019/MealViewController.swift
@@ -28,9 +28,16 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         
         // Handle the text field's user input through delegate callabacks.
         nameTextField.delegate = self
+        
+        // Enable the Save button only if the text field has a valid Meal name.
+        updateSaveButtonState()
     }
     
     // MARK: UITextFieldDelegate
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Disable the Save button while editing.
+        saveButton.isEnabled = false
+    }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         // Hide the keyboard.
         textField.resignFirstResponder()
@@ -38,6 +45,8 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
+        updateSaveButtonState()
+        navigationItem.title = textField.text
     }
     
     // MARK: Navigation
@@ -94,6 +103,13 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         
         //Dismiss the picker
         dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: Private Methods
+    private func updateSaveButtonState() {
+        // Disable the Save button if the text field is empty
+        let text = nameTextField.text ?? ""
+        saveButton.isEnabled = !text.isEmpty
     }
     
 }
